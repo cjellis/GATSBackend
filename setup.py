50b7dde6124a8ced1a385097a39b0b38762cb2fe
Craig Ellis <crellis@cisco.com>
2015-11-14 23:47:15 -0500

    added setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='GATSBackend',
+    version='1.0',
+    description='Backend Server',
+    author='Team GATS',
+    author_email='ellis.cr@husky.neu.edu',
+    url='https://www.python.org/sigs/distutils-sig/',
+    py_modules=['app', 'DBScripts']
+)
