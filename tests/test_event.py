ad013a5cf07e3cc89e53e1529ed6a09144c6f7b2
Craig Ellis <crellis@cisco.com>
2015-11-30 18:46:59 -0500

    added my events api for faculty

--- tests/test_event.py
+++ tests/test_event.py
@@ -674,3 +674,54 @@ class EventTestCase(unittest.TestCase):
 
         rv = self.app.post('/events/distributePoints/{}/{}'.format(event_id, self.token))
         assert "ERROR: attendance needs to be verified" in rv.data
+
+    def test_get_my_events(self):
+
+        rv = self.app.post('/events/addEvent/{}'.format(self.token),
+                           data=json.dumps({
+    "title": "Northeastern University Growth Opportunities for Asian American Leaders (NUGOAL)",
+    "format": "Training and development program",
+    "topics": ["Multicultural", "Leadership", "Community Engagement", "Exploring Identity"],
+    "description": "Northeastern University Growth and Opportunity for"
+                   " Asian American Leaders is a program specifically designed for first and second year "
+                   "Asian American students who are looking to increase and gain experiences to empower themselves as"
+                   " leaders at Northeastern University and beyond. A cohort of students will be chosen based on their "
+                   "potential as future leaders and need for leadership development. This seven week program will focus"
+                   " on the intersection of leadership and Asian American racial identity through discussions and"
+                   " projects. It will be facilitated by current Asian American student leaders and AAC staff. Apply by"
+                   " December 1, 2015.",
+    "begin": "12/1/2015",
+    "end": "3/1/2015",
+    "engagementLengthValue": 1,
+    "engagementLengthUnit": "Semester",
+    "recurrence": "Yearly",
+    "location": "Boston Campus",
+    "sponsoringDepartment": "Asian American Center",
+    "pointOfContact": {"name": "Kristine Din", "number": "6173735554", "email": "k.din@neu.edu"},
+    "outcomes": ["Students will be able to define leadership in relationship to their own racial identity.",
+                 "Students will be able to describe their leadership strengths.",
+                 "Students will be able to employ their leadership style and strengths in their daily lives.",
+                 "Students will be able to analyze leadership in the Asian American community.",
+                 "Students will be able to propose a meaningful intervention for building Asian American leadership"
+                 " capacity.",
+                 "Students will be able to assess the need for leadership development within the Asian American"
+                 " community at Northeastern."],
+    "skills": ["TestSkill"],
+    "engagementLevel": "Active",
+
+    "coopFriendly": True,
+    "academicStanding": ["First Year", "Second Year"],
+    "major": "Any",
+    "residentStatus": "both",
+    "otherRequirements": ["Identify as Asian American"],
+    "checkAttendance": False
+}), content_type='application/json')
+        assert "Success" in rv.data
+
+        rv = self.app.get('/events/getMyEvents/{}'.format(self.token))
+        obj = json.loads(rv.data)
+        assert len(obj["data"]) is 1
+
+        rv = self.app.get('/events/getMyEvents/{}'.format(self.token_student))
+        obj = json.loads(rv.data)
+        assert len(obj["data"]) is 0
